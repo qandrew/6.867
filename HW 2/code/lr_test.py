6133e7b707652ef8e6fbd398caad5d5a6b89a71c
andrewqandrew <axia@mit.edu>
2016-10-21 13:01:51 -0400

    starting hw2

--- HW 2/code/lr_test.py
+++ HW 2/code/lr_test.py
@@ -2,20 +2,28 @@ from numpy import *
 from plotBoundary import *
 import pylab as pl
 # import your LR training code
+from sklearn.linear_model import LogisticRegression
 
 # parameters
-name = 'ls'
+name = '1'
 print '======Training======'
 # load data from csv files
 train = loadtxt('data/data'+name+'_train.csv')
 X = train[:,0:2]
 Y = train[:,2:3]
 
+print len(X),X[0]
+print "X, Y"
+print len(Y), Y[0]
+
 # Carry out training.
-### TODO ###
+lr = LogisticRegression(penalty='l2', tol=0.0001, C=1000000.0, fit_intercept=True, intercept_scaling=1, solver='liblinear', max_iter=2000)
 
 # Define the predictLR(x) function, which uses trained parameters
 ### TODO ###
+lr.fit(X,Y)
+def predictLR(x):
+    return lr.predict_proba(x)[0,1]
 
 # plot training results
 plotDecisionBoundary(X, Y, predictLR, [0.5], title = 'LR Train')
