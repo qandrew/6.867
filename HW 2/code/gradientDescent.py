a7db99abc4527a3d485e0ad40b73e4b0f29d46cb
andrewqandrew <axia@mit.edu>
2016-10-22 16:01:05 -0400

    finished hw2.2

--- HW 2/code/gradientDescent.py
+++ HW 2/code/gradientDescent.py
@@ -11,56 +11,56 @@ def cost(X,y,theta,theta_0,L, debug = False):
     #compute J (cost) given theta
     reg = L * np.sum(np.square(theta))
     # theta_0 = np.ones((X.shape[0],1))*theta_0
-    # if debug:
-    #     print "\ndebug\n", np.exp(y[i]*(theta.T*X[i]+theta_0))
+    if debug:
+        print "\ndebug\n", np.exp(y[i]*(theta.T*X[i]+theta_0))
     tempCost = 0
-    for i in xrange(len(X)): #slow
+    for i in xrange(len(X)):
         tempCost += np.log(1 + np.exp(-y[i]*(X[i]*theta+theta_0)) )
     # cost = np.sum(np.log(1 + np.exp(-1*y.T*(X*theta + theta_0)))) + reg
+    # print "tempCost", tempCost
     return tempCost + reg
 
 # def updateThetaB(X,y,theta_old,iter,t,k):
-    # #update theta
-    # theta_new = np.zeros((X.shape[1],1)) #initiate theta at 0 #initiate theta_new to be 0s for now
-    # step = (t+iter)**(-k)
-    # # step = 1
-    # for j in xrange(10):
-    #     sum_error = np.sum((np.transpose(X[:,j])*(y-X*theta_old)))
-    #     theta_new[j] = theta_old[j] + step*sum_error
-    # return theta_new, cost(X,y,theta_new)
+#     #update theta
+#     theta_new = np.zeros((X.shape[1],1)) #initiate theta at 0 #initiate theta_new to be 0s for now
+#     step = (t+iter)**(-k)
+#     # step = 1
+#     for j in xrange(10):
+#         sum_error = np.sum((np.transpose(X[:,j])*(y-X*theta_old)))
+#         theta_new[j] = theta_old[j] + step*sum_error
+#     return theta_new, cost(X,y,theta_new)
 
-def updateThetaS(X,y,theta_old,theta_0_old,i,t,k,L):
+def updateThetaS(X,y,theta_old,theta_0_old,step,L):
     #update theta
     theta_new = np.zeros((X.shape[1],1)) #initiate theta at 0 #initiate theta_new to be 0s for now
-    step = (t+i)**(-k)
     i = randint(0,X.shape[1]-1)
     for j in xrange(len(theta_old)):
-        theta_new[j] = theta_old[j] + step*y[i,0]*X[i,j]/(1+np.exp(y[i,0]*(X[i]*theta_old+theta_0_old))) + step*L*theta_old[j]
-    theta_0_new = theta_0_old + step*y[i,0]*(1+np.exp(y[i,0]*(X[i]*theta_old+theta_0_old)))
+        theta_new[j] = theta_old[j] + step*y[i,0]*X[i,j]*1/(1+np.exp(y[i,0]*(X[i]*theta_old+theta_0_old))) - step*L*theta_old[j]
+    theta_0_new = theta_0_old + step*y[i,0]*1/(1+np.exp(y[i,0]*(X[i]*theta_old+theta_0_old)))
     return theta_new, theta_0_new, cost(X,y,theta_new,theta_0_new,L)
 
 # def gradientDescentBatch(X,y, theta, t,k, conv):
-    # #X: given X data
-    # #y: given y data
-    # #step: step size
-    # #conv: convergence criterion
-    # #goal: solve theta
-    # theta_new = theta
-    # cost_new = cost(X,y,theta_new)
-    # # print "starting cost", cost_new
-    # cost_old = cost_new + 2*conv
-    # i = 0 #iterator counts
-    # while abs(cost_old - cost_new) >= conv: #while we have not converged
-    #     cost_old = cost_new
-    #     theta_old = theta_new
-    #     theta_new, cost_new = updateThetaB(X,y,theta_old,i,t,k)
-    #     i += 1
-    #     print "iteration:", i, "cost:", cost_new, "diff:", cost_old - cost_new
-    #     # print "theta", theta_new
+#     #X: given X data
+#     #y: given y data
+#     #step: step size
+#     #conv: convergence criterion
+#     #goal: solve theta
+#     theta_new = theta
+#     cost_new = cost(X,y,theta_new)
+#     # print "starting cost", cost_new
+#     cost_old = cost_new + 2*conv
+#     i = 0 #iterator counts
+#     while abs(cost_old - cost_new) >= conv: #while we have not converged
+#         cost_old = cost_new
+#         theta_old = theta_new
+#         theta_new, cost_new = updateThetaB(X,y,theta_old,i,t,k)
+#         i += 1
+#         print "iteration:", i, "cost:", cost_new, "diff:", cost_old - cost_new
+#         # print "theta", theta_new
+#
+#     return theta_new
 
-    # return theta_new
-
-def gradientDescentStochastic(X,y, theta, theta_0, t, k, conv, L):
+def gradientDescentStochastic(X,y, theta, theta_0, step, conv, L):
     #X: given X data
     #y: given y data
     #step: step size
@@ -79,16 +79,16 @@ def gradientDescentStochastic(X,y, theta, theta_0, t, k, conv, L):
     cost_new = cost(X,y,theta_new,theta_new_0,L)
     # print "starting cost", cost_new
     cost_old = cost_new + 2*conv
-    i = 0 #iterator counts
+    i = 1 #iterator counts
     while abs(cost_old - cost_new) >= conv: #while we have not converged
         cost_old = cost_new
         theta_old = theta_new
         theta_old_0 = theta_new_0
-        theta_new, theta_new_0, cost_new = updateThetaS(X,y,theta_old,theta_old_0,i,t,k,L)
+        theta_new, theta_new_0, cost_new = updateThetaS(X,y,theta_old,theta_old_0,step,L)
         i += 1
         #print "iteration:", i, "cost:", cost_new, "diff:", cost_old - cost_new, "weights:", theta_new, theta_new_0
-        if i%200 == 0:
-            print "iteration:", i, "cost:", cost_new, "weights:", theta_new, theta_new_0
+        if ((i-1)%200 == 0):
+            print "iteration:", i-1, "cost:", cost_new, "weights:", theta_new, theta_new_0
         # print "theta", theta_new
         theta_0s.append(theta_new_0)
         theta_1s.append(theta_new[0,0])
@@ -107,38 +107,14 @@ if __name__ == "__main__":
 
     theta = np.zeros((X.shape[1],1)) #initiate theta at 0, last element is w0
     theta_0 = 0
-    step = 0.0002 #specify step here
-    t = 100000 #specify variable step size
-    k = 0.7
-    conv = 0.000001 #specify convergence here
+    step = 1 #specify step here
+    conv = 0.0001 #specify convergence here
 
-    # print cost(X,Y,np.matrix([[-1.21588605],[11.73331597]]),6.15088691,0)
+    # print cost(X,Y,[[-0.57],[6.322]],3.25,0)
 
     ### Lambda = 0 ###
-    L = 0
-    theta_f,theta_f_0, theta_0s, theta_1s, theta_2s = gradientDescentStochastic(X,Y,theta,theta_0,t,k,conv,L)
-
-    print X.shape
-    print Y.shape
-    print X[0], Y[0]
-    print len(theta_f), len(theta_f[0])
-    print theta_f.shape, theta_f_0.shape
-
-    print "initial cost", cost(X,Y,theta,theta_0,L)
-    print "final theta & cost", theta_f, theta_f_0, cost(X,Y,theta_f,theta_f_0,L,debug=True)
-
-    x, = plt.plot(theta_0s,label="w0")
-    y, = plt.plot(theta_1s,label="w1")
-    z, = plt.plot(theta_2s,label="w2")
-    plt.xlabel('iterations')
-    plt.ylabel('weight value')
-    plt.title("lambda=" + str(0))
-    # plt.legend(handles=[x,y,z],loc=2)
-    plt.show()
-
-    ### Lambda = 1 ###
-    # L = 1
-    # theta_f,theta_f_0, theta_0s, theta_1s, theta_2s = gradientDescentStochastic(X,Y,theta,theta_0,t,k,conv,L)
+    # L = 0
+    # theta_f,theta_f_0, theta_0s, theta_1s, theta_2s = gradientDescentStochastic(X,Y,theta,theta_0,step,conv,L)
     # print "initial cost", cost(X,Y,theta,theta_0,L)
     # print "final theta & cost", theta_f, theta_f_0, cost(X,Y,theta_f,theta_f_0,L)
 
@@ -147,6 +123,23 @@ if __name__ == "__main__":
     # z, = plt.plot(theta_2s,label="w2")
     # plt.xlabel('iterations')
     # plt.ylabel('weight value')
-    # plt.title("lambda=" + str(1))
-    # # plt.legend(handles=[x,y,z],loc=2)
+    # plt.title("lambda=" + str(0))
+    # plt.legend(
+    #     [x,y,z],loc=2)
     # plt.show()
+
+    ### Lambda = 0 ###
+    L = 1
+    print "Lambda", 1
+    theta_f,theta_f_0, theta_0s, theta_1s, theta_2s = gradientDescentStochastic(X,Y,theta,theta_0,0.0001,conv,L)
+    print "initial cost", cost(X,Y,theta,theta_0,L)
+    print "final theta & cost", theta_f, theta_f_0, cost(X,Y,theta_f,theta_f_0,L)
+
+    x, = plt.plot(theta_0s,label="w0")
+    y, = plt.plot(theta_1s,label="w1")
+    z, = plt.plot(theta_2s,label="w2")
+    plt.xlabel('iterations')
+    plt.ylabel('weight value')
+    plt.title("lambda=" + str(1))
+    plt.legend([x,y,z],loc=2)
+    plt.show()
