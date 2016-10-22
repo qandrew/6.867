b242d853c89c8bb5fdc692d24f80ebba08d5d994
andrewqandrew <axia@mit.edu>
2016-10-22 19:08:59 -0400

    HW 2.3.3 skeleton code implemented, to test

--- HW 2/code/pegasos_gaussian_test.py
+++ HW 2/code/pegasos_gaussian_test.py
@@ -1,28 +1,59 @@
-from numpy import *
+import numpy as np
 from plotBoundary import *
 import pylab as pl
 # import your LR training code
 
 # load data from csv files
 train = loadtxt('data/data3_train.csv')
-X = train[:,0:2]
+X = np.matrix(train[:,0:2]) #formulate into NP matrix
 Y = train[:,2:3]
+n = X.shape[0]
 
 # Carry out training.
 epochs = 1000;
-l = .02; #lambda
+lmbda = .02; #lambda
 gamma = 2e-2;
 
+def kernel_gaussian(a,b,gamma):
+	power = np.linalg.norm(a - b)**2
+	return e**(-gamma*power)
+
 K = zeros((n,n));
+K_sums = zeros((n,1)) # sum_j K(x_i, x_j)
 ### TODO: Compute the kernel matrix ###
+for i in xrange(n):
+	for j in xrange(n):
+		K[i,j] = kernel_gaussian(X[i],X[j],gamma)
+	K_sums[i] = np.sum(K[i,:])
 
 ### TODO: Implement train_gaussianSVM ###
-alpha = train_gaussianSVM(X, Y, K, lmbda, epochs);
+def train_gaussianSVM(X,Y,K_sums,l,epochs):
+	t = 0
+	alpha = np.zeros((X.shape[0],1))	
+	for epoch in xrange(epochs):
+		for i in xrange(n):
+			t += 1
+			step = 1/(t*l)
+			if (Y[i]* alpha[i]*K_sums[i])[0] < 1:
+				alpha[i] = (1 - step)*alpha[i] + step*Y[i]
+			else:
+				alpha[i] = (1 - step)*alpha[i]
+	return alpha
 
+alpha = train_gaussianSVM(X, Y, K_sums, lmbda, epochs);
 
-# Define the predict_gaussianSVM(x) function, which uses trained parameters, alpha
-### TODO:  define predict_gaussianSVM(x) ###
+print alpha
 
-# plot training results
+# # Define the predict_gaussianSVM(x) function, which uses trained parameters, alpha
+# ### TODO:  define predict_gaussianSVM(x) ###
+def predict_gaussianSVM(x):
+	toret = 0
+	for i in xrange(n):
+		# https://www.quora.com/What-is-the-intuition-behind-Gaussian-kernel-in-SVM
+		toret += alpha[i]*kernel_gaussian(x,X[i],gamma)*Y[i]
+	return toret
+
+# # plot training results
+print '======PLOTTING======'
 plotDecisionBoundary(X, Y, predict_gaussianSVM, [-1,0,1], title = 'Gaussian Kernel SVM')
 pl.show()
