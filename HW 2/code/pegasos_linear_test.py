cedf1f69100feea5dbfc3985a84e424e8ef386bb
andrewqandrew <axia@mit.edu>
2016-10-24 23:11:05 -0400

    modified to augment w

--- HW 2/code/pegasos_linear_test.py
+++ HW 2/code/pegasos_linear_test.py
@@ -5,45 +5,44 @@ import pylab as pl
 
 # load data from csv files
 train = loadtxt('data/data3_train.csv')
+Xplot = train[:,0:2]
 X = np.matrix(train[:,0:2]) #formulate into NP matrix
-Y = train[:,2:3]
 n = X.shape[0] #height of X
+Xwide = np.hstack((np.ones((n,1)),X))
+Y = train[:,2:3]
 
 power = -10
 l = 2**power #lambda value
-C = 1/(l*n) #margin tolerance
+C = 1.0/(l*n) #margin tolerance
 epoch = 0
 max_epochs = 1000
-w = np.zeros((X[0].shape[1]+1, 1))
-# w0 = 0 #the bias
+w = np.zeros((1, Xwide.shape[1])) #w[0] is the bias
 t = 0
 
 # Carry out training.
 while epoch < max_epochs:
+	if epoch%100 == 0: print "Epoch", epoch, w
 	for i in xrange(n):
 		t += 1
-		step = 1/(t*l)
-
-		if Y[i]*(X[i]*w[0:2,:] + w[2])[0,0] < 1: #if incorrectly classified, no bias
-		# if Y[i]*(X[i]*w + w0)[0,0] < 1: #if incorrectly classified
-			w[0:2,:] = (1 - step*l)*w[0:2,:] + (step*Y[i]*X[i]).T 
-			w[2] = (step*Y[i]).T
-			# w0 = (1 - step*l)*w0 + (step*Y[i]).T #separate the bias term
+		step = 1.0/(t*l)
+		w_unbiased = np.copy(w)
+		w_unbiased[0][0] = 0
+		if Y[i]*np.dot(w,Xwide[i].T) < 1: #incorrect classify
+			w = w- step*l*w_unbiased + step*Y[i]*Xwide[i]
 		else: #correctly classified
-			w[0:2,:] = (1 - step*l)*w[0:2,:]
-			# w0 = (1 - step*l)*w0
+			w = w- step*l*w_unbiased
 	epoch += 1
 
 # Define the predict_linearSVM(x) function, which uses global trained parameters, w
 ### TODO: define predict_linearSVM(x) ###
 print 'w', w
-# print 'w0', w0
 print 'lambda power', power
 
 def predict_linearSVM(x):
-	return w[0]*x[0] + w[1]*x[1] + w[2] # + w0 #w0 is the bias
+	x = np.concatenate(([1],x))
+	return np.dot(w,x.T)[0][0]
 
 # plot training results
 print '======PLOTTING======'
-plotDecisionBoundary(X, Y, predict_linearSVM, [-1,0,1], title = 'Linear SVM lambda = 2^'+str(power))
+plotDecisionBoundary(Xplot, Y, predict_linearSVM, [-1,0,1], title = 'Linear SVM lambda = 2^'+str(power))
 pl.show()
